0bec15bc18162989a0267b1c9036ae32c674a9c6
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-06-06 15:50:35 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../pages/About';
 
-// primeiro commit
+describe('Testando o componente About.js', () => {
+  it('Verifica se contém um heading h2 como o texto "About Pokedéx"', () => {
+    render(<About />);
+
+    const headingPokedex = screen.getByRole('heading',
+      { name: 'About Pokédex', level: 2 });
+    expect(headingPokedex).toBeInTheDocument();
+  });
+
+  it('Verifica contém dois parágrafos como o texto sobre Pokédex"', () => {
+    render(<About />);
+
+    const twoParagraphs = screen.getAllByText(/Pokémons/i);
+    expect(twoParagraphs).toHaveLength(2);
+  });
+
+  it('Verifica se contém imagem com a uma URL específica', () => {
+    render(<About />);
+
+    const image = screen.getByAltText('Pokédex');
+    expect(image.src)
+      .toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
